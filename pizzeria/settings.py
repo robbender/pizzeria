782253fc0a75641f2a4e7aeef63c30c14b0be71d
Robert Bender <robb.webdev@gmail.com>
2022-10-14 14:33:58 -0700

    upd settings

--- pizzeria/settings.py
+++ pizzeria/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5cc=*0eb+2&@3^87(b!t78&66w+$b!@+yw+sgy&w4mnnvb(y5@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["robb-pizzaria.herokuapp.com", "127.0.0.1"]
-CSRF_TRUSTED_ORIGINS = ["https://robb-pizzaria.herokuapp.com"]
+# CSRF_TRUSTED_ORIGINS = ["https://robb-pizzaria.herokuapp.com"]
 
 # ALLOWED_HOSTS = []
 
