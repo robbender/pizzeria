0841b7a8d348513d1849ada7e9f0520860620d41
Robert Bender <robb.webdev@gmail.com>
2022-10-14 16:08:13 -0700

    rm django on heroku import

--- pizzeria/settings.py
+++ pizzeria/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import django_on_heroku
+# import django_on_heroku
 from pathlib import Path
 import os
 
